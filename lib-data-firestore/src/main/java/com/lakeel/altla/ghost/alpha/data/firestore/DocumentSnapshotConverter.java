a42e6b42fa2ef8f05e825c5dc860a49b9900634b
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2017-10-17 19:23:22 +0900

    [REFACTOR] Remove unused classes

--- lib-data-firestore/src/main/java/com/lakeel/altla/ghost/alpha/data/firestore/DocumentSnapshotConverter.java
+++
@@ -1,8 +0,0 @@
-package com.lakeel.altla.ghost.alpha.data.firestore;
-
-import com.google.firebase.firestore.DocumentSnapshot;
-
-public interface DocumentSnapshotConverter<T> {
-
-    T convert(DocumentSnapshot snapshot);
-}
