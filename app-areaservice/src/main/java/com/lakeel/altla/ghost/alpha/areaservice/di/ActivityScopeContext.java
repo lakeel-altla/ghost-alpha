c4a3b9b07cfd920b90b9c6aedfd0cdf54c7474ca
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2017-10-19 18:33:06 +0900

    [REMOVE] Remove app-areaservice module

--- app-areaservice/src/main/java/com/lakeel/altla/ghost/alpha/areaservice/di/ActivityScopeContext.java
+++
@@ -1,8 +0,0 @@
-package com.lakeel.altla.ghost.alpha.areaservice.di;
-
-import com.lakeel.altla.ghost.alpha.areaservice.di.component.ActivityComponent;
-
-public interface ActivityScopeContext {
-
-    ActivityComponent getActivityComponent();
-}
--- app-areaservice/src/main/java/com/lakeel/altla/ghost/alpha/areaservice/di/module/Names.java
+++
@@ -1,9 +0,0 @@
-package com.lakeel.altla.ghost.alpha.areaservice.di.module;
-
-public final class Names {
-
-    public static final String GOOGLE_API_KEY = "googleApiKey";
-
-    private Names() {
-    }
-}
