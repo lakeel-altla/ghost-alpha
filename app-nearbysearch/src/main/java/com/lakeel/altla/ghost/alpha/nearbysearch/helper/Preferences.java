15daf848c8ef4db35282bccb2996f4b4a9ab79db
Atsuhiro Nabeta <nabeta-a@legendapl.com>
2017-10-23 14:17:11 +0900

    [UPDATE] Show location picker on long click of fab to update location manualy

--- app-nearbysearch/src/main/java/com/lakeel/altla/ghost/alpha/nearbysearch/helper/Preferences.java
+++ app-nearbysearch/src/main/java/com/lakeel/altla/ghost/alpha/nearbysearch/helper/Preferences.java
@@ -14,8 +14,6 @@ public final class Preferences {
 
     public static final String KEY_LOCATION_UPDATES_INTERVAL = "location_update_interval";
 
-    public static final String KEY_MANUAL_LOCATION_UPDATES_ENABLED = "manual_location_updates_enabled";
-
     public static final String KEY_LOCATION_REQUEST_PRIORITY = "location_request_priority";
 
     public static final String KEY_PLACE_DETAILS_VIEW_ENABLED = "place_details_view_enabled";
@@ -43,10 +41,6 @@ public final class Preferences {
 
     public int getLocationUpdatesInterval() {
         return Integer.parseInt(preferences.getString(KEY_LOCATION_UPDATES_INTERVAL, null));
-    }
-
-    public boolean isManualLocationUpdatesEnabled() {
-        return preferences.getBoolean(KEY_MANUAL_LOCATION_UPDATES_ENABLED, false);
     }
 
     public int getLocationRequestPriority() {
--- app-nearbysearch/src/main/java/com/lakeel/altla/ghost/alpha/nearbysearch/view/fragment/NearbyPlaceListFragment.java
+++ app-nearbysearch/src/main/java/com/lakeel/altla/ghost/alpha/nearbysearch/view/fragment/NearbyPlaceListFragment.java
@@ -121,37 +121,38 @@ public final class NearbyPlaceListFragment extends Fragment {
 
         FloatingActionButton floatingActionButton = findViewById(this, R.id.fab);
         floatingActionButton.setOnClickListener(v -> {
-            if (preferences.isManualLocationUpdatesEnabled()) {
-                LocationPickerActivity.Builder builder = new LocationPickerActivity.Builder(getContext())
-                        .setMyLocationEnabled(true)
-                        .setBuildingsEnabled(false)
-                        .setIndoorEnabled(true)
-                        .setTrafficEnabled(false)
-                        .setMapToolbarEnabled(false)
-                        .setZoomControlsEnabled(true)
-                        .setMyLocationButtonEnabled(true)
-                        .setCompassEnabled(true)
-                        .setIndoorLevelPickerEnabled(true)
-                        .setAllGesturesEnabled(true);
-
-                if (location != null) {
-                    builder.setLocation(location.latitude, location.longitude);
-                }
-
-                Intent intent = builder.build();
-                startActivityForResult(intent, REQUEST_CODE_LOCATION_PICKER);
+            Location lastLocation = fragmentContext.getLastLocation();
+            if (lastLocation == null) {
+                LOG.w("The last location could not be resolved.");
+                location = null;
+                LOG.w("Trying to check location settings.");
+                fragmentContext.checkLocationSettings();
             } else {
-                Location lastLocation = fragmentContext.getLastLocation();
-                if (lastLocation == null) {
-                    LOG.w("The last location could not be resolved.");
-                    location = null;
-                    LOG.w("Trying to check location settings.");
-                    fragmentContext.checkLocationSettings();
-                } else {
-                    location = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
-                    searchPlaces();
-                }
+                location = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                searchPlaces();
             }
+        });
+        floatingActionButton.setOnLongClickListener(v -> {
+            LocationPickerActivity.Builder builder = new LocationPickerActivity.Builder(getContext())
+                    .setMyLocationEnabled(true)
+                    .setBuildingsEnabled(false)
+                    .setIndoorEnabled(true)
+                    .setTrafficEnabled(false)
+                    .setMapToolbarEnabled(false)
+                    .setZoomControlsEnabled(true)
+                    .setMyLocationButtonEnabled(true)
+                    .setCompassEnabled(true)
+                    .setIndoorLevelPickerEnabled(true)
+                    .setAllGesturesEnabled(true);
+
+            if (location != null) {
+                builder.setLocation(location.latitude, location.longitude);
+            }
+
+            Intent intent = builder.build();
+            startActivityForResult(intent, REQUEST_CODE_LOCATION_PICKER);
+
+            return true;
         });
     }
 
